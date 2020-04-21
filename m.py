1fae4a5e335ed1a2c2ef6497b3c9a9b53d65ffa8
RealVirg <49589317+RealVirg@users.noreply.github.com>
2020-04-21 20:27:12 +0500

    Add files via upload

--- m.py
+++ m.py
@@ -1,9 +1,11 @@
 ip = ""
 
+# ввод с файла
 with open('in.txt') as f:
     ip = f.read().split('\n')
 
 
+# класс чисто для моего удобства хранения коня и пешки
 class Figure:
     def __init__(self, name, x, y):
         self.name = name
@@ -14,6 +16,8 @@ class Figure:
         return self.name + '(' + str(dictionary_2[self.x]) + ', ' + str(self.y) + ')'
 
 
+# Здесь идет проверка на выход за пределы поля, попадания под удар и на посещенность в прошлые разы,
+# так же кладет следующий шаг в стек + в посещенные + односвязный список
 def checker(point, v, p, c, pa):
     checkers = [(2, -1), (2, 1), (1, 2), (1, -2), (-2, -1), (-2, 1), (-1, 2), (-1, -2)]
     for check in checkers:
@@ -26,6 +30,7 @@ def checker(point, v, p, c, pa):
             c[b] = point
 
 
+# Восстановление пути по односвязному списку
 def chain(point, c):
     a = [point]
     while c[point] != -1:
@@ -37,26 +42,32 @@ def chain(point, c):
 
 dictionary = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8}
 dictionary_2 = {1: 'a', 2: 'b', 3: 'c', 4: 'd', 5: 'e', 6: 'f', 7: 'g', 8: 'h'}
+
 horse = Figure('horse', ip[0][0], ip[0][1])
 pawn = Figure('pawn', ip[1][0], ip[1][1])
+
 print(horse)
 print(pawn)
-chaining_list = {(horse.x, horse.y): -1}
-visited = [(horse.x, horse.y)]
-pool = [(horse.x, horse.y)]
 
+chaining_list = {(horse.x, horse.y): -1}  # односвязный список для восстановления пути
+visited = [(horse.x, horse.y)]  # лист посещенных
+pool = [(horse.x, horse.y)]  # стек
+
+# основной цикл
 while len(pool) > 0:
     # print(pool)
     current = pool.pop()
-    if current[0] == pawn.x and current[1] == pawn.y:
+    if current[0] == pawn.x and current[1] == pawn.y:  # вытащенный шаг из стека = позиции пешки
         print('good')
-        answer = chain(current, chaining_list)
+        answer = chain(current, chaining_list)  # восстановление пути
         print(answer)
         with open('out.txt', 'w') as f:
             for po in answer:
-                f.write(dictionary_2[po[0]] + str(po[1]) + "\n")
+                f.write(dictionary_2[po[0]] + str(po[1]) + "\n")  # вывод в файл
         break
+    # проверка возможных следующих ходов + добавление их в стек, лист посещенных, односвязный список
     checker(current, visited, pool, chaining_list, pawn)
+
 print('bad')
 
 
